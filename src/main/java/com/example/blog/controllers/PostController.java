356a7c5c484580196c95acc5357187a8a707bb44
Lynette <lyna8907@gmail.com>
2018-04-16 16:02:55 -0500

    completed spring controllers exercise - created controllers for home, math, and posts

---
+++ src/main/java/com/example/blog/controllers/PostController.java
@@ -0,0 +1,35 @@
+package com.example.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String showPosts(){
+        return "posts index page";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String viewIndividualPost(@PathVariable long id){
+        return "viewing post # " + id;
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String viewCreatePost(){
+        return "view the form for creating a post";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String createPost(){
+        return "created a new post";
+    }
+}
